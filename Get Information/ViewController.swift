d391a3dc27e989841dedbe554fc1350b8d80063d
shakhawat hossain <shakhwt@gmail.com>
2020-12-07 18:29:35 +0600

    Initial Commit

---
+++ Get Information/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Get Information
+//
+//  Created by Shakhawat Hossain Shakib on 7/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
